e8b96dde8c64532fca40257e7c62b4f6d7ac8a2b
ItzDaniell <juan.rodriguez.o@tecsup.edu.pe>
2024-10-29 08:13:50 -0500

    Prueba

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,7 +4,133 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <style>@import url('https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap');
+        * {
+            box-sizing: border-box;
+            padding: 0;
+            margin: 0;
+            font-family: Nunito, sans-serif;
+            font-size: 17px;
+        }
+        body {
+            background-color: white;
+        }
+        a{
+            text-decoration: none;
+        }
+        .header{
+            background-color: #F3AF31;
+            display: flex;
+            justify-content: flex-end;
+            align-items: center;
+            height: 70px;
+            padding: 5px 7%;
+        }
+        .header .logo{
+            display: flex;
+            cursor: pointer;
+            margin-right: auto;
+            align-items: center;
+        }
+        .header .logo img{
+            height: 50px;
+            width: auto;
+            padding-right: 10px;
+            transition: all 0.3s;
+        }
+        .header .logo img:hover{
+            transform: scale(1.2);
+        }
+        .header .logo a{
+            color: white;
+            vertical-align: auto;
+        }
+        .nav-links li {
+            list-style: none;
+        }
+        .header .nav-links li{
+            display: inline-block;
+            padding: 0 15px;
+            transition: all 0.3s;
+        }
+        .nav-links a{
+            color: white;
+        }
+        .header .nav-links li:hover{
+            transform: scale(1.1);
+        }
+        .header .nav-links a:hover{
+            color: #212121;
+        }
+        .link-user button{
+            border:none;
+            background: white;
+            color: #212121;
+            padding: 9px 12px;
+            border-radius: 15px;
+            cursor: pointer;
+            transition: all 0.3s ease 0s;
+        }
+        .link-user button:hover{
+            background:#212121;
+            color: white;
+            transform: scale(1.1);
+        }
+        .link-user a{
+            padding-left: 15px;
+        }
+        .devshare{
+            display: flex;
+            font-weight:300;
+            padding-top: 100px;
+            justify-content: center;
+            height: 555px;
+        }
+        .devshare h1{
+            font-size: 60px;
+        }
+        
+        .presentacion{
+            display: flex;
+            height: 680px;
+        }
+        .text{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 50%;
+        }
+        .text h2{
+            font-size: 40px;
+        }
+        .text h1{
+            font-size: 64px;
+        }
+        .presentacion button{
+            border:none;
+            background: #212121;
+            color: white;
+            padding: 9px 12px;
+            border-radius: 15px;
+            cursor: pointer;
+        }
+        
+        .presentacion-img{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 50%;
+        }
+        .presentacion img{
+            width: 400px;
+            height: 450px;
+        }
+        .parrafo{
+            padding-top:20px;
+            padding-bottom: 20px;
+            width: 400px;
+        }
+    </style>
     <title>DevShare - @yield('title')</title>
 </head>
 <body>
